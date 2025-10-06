9a758439504ef770b3adffe197d585f54b885085
Blinkx-Original <martinpino@outlook.com>
2025-10-06 11:00:45 +0200

    fix: load server manifest before vercel handler

--- modules.d.ts
+++ modules.d.ts
@@ -2,3 +2,9 @@ declare module "*.toml" {
   const value: any;
   export default value;
 }
+
+declare module '@astrojs/vercel/serverless' {
+  import type { AstroIntegration } from 'astro';
+  function vercel(options?: Record<string, unknown>): AstroIntegration;
+  export default vercel;
+}
